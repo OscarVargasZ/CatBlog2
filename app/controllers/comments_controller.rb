71ad91b1bbdd1ec358fd8423e8d17cc1309e801c
oscar <oscar.vargas@mayor.cl>
2020-02-07 17:08:08 -0300

    solving dat error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,31 @@ class CommentsController < ApplicationController
         @comment = @article.comments.create(comment_params)
         @comment.update(user_id:current_user.id)
         redirect_to article_path(@article)
-      end
-     
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        if(@comment.user_id == current_user.id)
+            @comment.destroy
+        end
+        redirect_to article_path(@article)
+    end
+    def edit
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @comment = @article.comments.find(params[:id])
+        if @article.comment.user_id == current_user.id
+                if @comment.update(comment_params) 
+                    redirect_to article_path(@article)
+                end
+            end
+        redirect_to 'edit'
+    end
+
       private
         def comment_params
           params.require(:comment).permit(:commenter, :body)
