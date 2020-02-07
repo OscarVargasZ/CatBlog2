e05be34a7aec7f38530489d77e6ffa94a5a5f45a
oscar <oscar.vargas@mayor.cl>
2020-02-07 02:09:10 -0300

    Implement partial view in article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,46 @@
 class ArticlesController < ApplicationController
+    def _index
+        @articles = Article.all
+      end
+    
+      def new
+        @article = Article.new
+      end
+     
+      def edit
+        @article = Article.find(params[:id])
+      end
+     
+      def create
+        @article = Article.new(article_params)
+        @article.update(user_id:current_user.id)
+     
+        if @article.save
+          redirect_to welcome_index_path
+        else
+          render 'new'
+        end
+      end
+     
+      def update
+        @article = Article.find(params[:id])
+     
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
+     
+      def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+     
+        redirect_to articles_path
+      end
+     
+      private
+        def article_params
+          params.require(:article).permit(:title, :text)
+        end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+    before_action :a
+    
+    def def index
+    end
+
+    private
+    def a
+        @articles = Article.all
+    end
 end
