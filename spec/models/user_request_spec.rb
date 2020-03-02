0730307ce506434ad570559dff976b4d02140afc
OscarVargasZ <oscar.vargas@mayor.cl>
2020-03-02 12:18:51 -0300

    complete model rspec

--- spec/models/user_request_spec.rb
+++ spec/models/user_request_spec.rb
@@ -1,1 +1,10 @@
 
+require 'rails_helper'
+
+RSpec.describe UserRequest, type: :model do
+    context 'RelationShips' do
+        it {should belong_to(:user)}
+        it {should belong_to(:request)}
+    #    it {should have_many(:requests)}
+    end
+end
